49d24e38afc886ea449772157b8ff9e2265fb2ab
Craig Mesdag <Craig.Mesdag@gmail.com>
2008-11-24 18:11:00 -0500

    Implemented the TF2 shader!!!

---
+++ MobRules/Content/Shaders/Illustrative.fx
@@ -0,0 +1,135 @@
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+float3   Camera;
+float4   Light;
+float4   LightColour;
+float    Intensity;
+
+// Texture Options
+texture  Texture, WarpTexture, SpecTexture, SpecMaskTexture;
+bool TextureEnabled;
+
+sampler textureSampler = sampler_state
+{
+  Texture   = (Texture);
+  Minfilter = LINEAR;
+  Magfilter = LINEAR;
+  Mipfilter = LINEAR;
+};
+
+sampler warpTextureSampler = sampler_state
+{
+  Texture = (WarpTexture);
+  Minfilter = LINEAR;
+  Magfilter = LINEAR;
+  Mipfilter = LINEAR;
+};
+
+sampler specTextureSampler = sampler_state
+{
+  Texture = (SpecTexture);
+  Minfilter = LINEAR;
+  Magfilter = LINEAR;
+  Mipfilter = LINEAR;
+};
+
+sampler specMaskTextureSampler = sampler_state
+{
+  Texture = (SpecMaskTexture);
+  Minfilter = LINEAR;
+  Magfilter = LINEAR;
+  Mipfilter = LINEAR;
+};
+
+struct VSInput
+{
+  float4 Position : POSITION0;
+  float3 Normal   : NORMAL0;
+  float2 UV       : TEXCOORD0;
+};
+
+struct VSOutput
+{
+  float4 Position  : POSITION;  // Position for view and camera
+  float3 XPosition : TEXCOORD0; // Transformed Position
+  float2 Tex       : TEXCOORD1; // UV data
+  float3 Normal    : TEXCOORD2; // Normalized normal in world space
+  float4 Amb       : COLOR0;
+};
+
+// Get the Background Light
+float4 GetAmbientLight()
+{
+  float4 colour = {1.0f, 1.0f, 1.0f, 1.0f};
+  float intensity = 0.6f;
+  return colour * intensity;
+}
+
+float4 W(float4 input)
+{
+  return tex2D(warpTextureSampler, input);
+}
+
+VSOutput VertexShaderFunction(VSInput input)
+{
+    VSOutput output;
+    
+    output.Position  = mul(mul(mul(input.Position, World), View), Projection);
+    output.XPosition = mul(input.Position, World);
+    output.Normal    = normalize(mul(input.Normal, (float3x3)World));
+    output.Tex       = input.UV;
+    output.Amb       = GetAmbientLight();
+
+    return output;
+}
+
+float4 PixelShaderFunction(VSOutput input) : COLOR0
+{
+    // View Independent Terms
+    float4 kd = tex2D(textureSampler, input.Tex);
+    
+    float4 lightColour = LightColour;
+    float3 unitView = normalize(Camera - input.XPosition);
+    float3 unitNorm = input.Normal; // N
+    float3 unitLightDir = normalize(Light - input.XPosition); // L
+    
+    float a = 0.5f; // From Gooch
+    float b = 0.5f; // From Gooch
+    int y   = 1;    // From p19-mitchell
+    
+    float4 viewIndependent = kd * (input.Amb + lightColour * W( pow((a * dot(unitNorm, unitLightDir) + b), y)));
+    
+    // View Dependent Terms
+    float cosA = dot(unitNorm, unitLightDir); // (N.L)
+    float3 r = normalize(2 * cosA * unitNorm - unitLightDir); // R = 2 * (N.L) * N - L
+    float3 up = {0.0f, 1.0f, 0.0f};
+    
+    float4 ks = tex2D(specTextureSampler, input.Tex); // Specular
+    float kSpecMask = tex2D(specMaskTextureSampler, input.Tex)[0];
+    //float4 kr = tex2D(rimTextureSampler, input.Tex); // Rim Highlight mask (Optional for Problem Cases)
+    
+    float cosB = dot(unitView, r);
+    
+    // Rim Highlight
+    float4 fs = pow(1.4 - dot(unitNorm, unitView), 4);
+    float4 fr = pow(1 - dot(unitNorm, unitView), 4);
+    
+    float4 viewDependent = lightColour * ks * max(fs * pow(cosB, kSpecMask), fr * pow(cosB, 2));
+    viewDependent += dot(unitNorm, up) * fr * (input.Amb, 0.3f);
+    
+    return viewIndependent + viewDependent;
+}
+
+technique Technique1
+{
+    pass Pass1
+    {
+        sampler[0] = (textureSampler);
+        sampler[1] = (warpTextureSampler);
+        sampler[2] = (specTextureSampler);
+        sampler[3] = (specMaskTextureSampler);
+        VertexShader = compile vs_2_0 VertexShaderFunction();
+        PixelShader = compile ps_2_0 PixelShaderFunction();
+    }
+}
--- MobRules/Game1.cs
+++ MobRules/Game1.cs
@@ -29,8 +29,13 @@ namespace MobRules
         InputManager inputManager = null;
         Camera camera = null;
 
-        // Shaders
-        PhongEffect phongEffect;
+        // Diffuse Warping Colour and Specular Highlight Texture
+        Texture2D warpTexture, specTexture, specMaskTexture;
+
+        // Test Ship
+        Model ship, defShip;
+        Vector3 shipPos;
+        Vector2 shipRot;
 
         // Test Cube
         Vector3 cubePos;
@@ -67,6 +72,10 @@ namespace MobRules
             graphics.PreferMultiSampling = true;
             graphics.ApplyChanges();
 
+            // Setup Test Ship
+            shipPos = new Vector3(-4.0f, -2.5f, -25.0f);
+            shipRot = new Vector2(90.0f, 0.0f);
+
             // Setup Test Cube.
             cubePos = new Vector3(0.0f, 0.0f, -5.0f);
             cubeRot = Vector2.Zero;
@@ -79,9 +88,6 @@ namespace MobRules
             camera = new Camera(this);
             ServiceHelper.Add<ICameraService>(camera);
             this.Components.Add(camera);
-
-            phongEffect = new PhongEffect();
-            phongEffect.Initialize();
 
             base.Initialize();
         }
@@ -166,13 +172,70 @@ namespace MobRules
         }
 
         /// <summary>
+        /// Alters a model so it will draw using a custom effect, while preserving
+        /// whatever textures were set on it as part of the original effects.
+        /// </summary>
+        private void ChangeModelEffect(Model model, Effect replacementEffect)
+        {
+            // Table mapping the original effects to our replacement versions.
+            Dictionary<Effect, Effect> effectMapping = new Dictionary<Effect, Effect>();
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                // Scan over all the effects currently on the mesh.
+                foreach (BasicEffect oldEffect in mesh.Effects)
+                {
+                    // If we haven't already seen this effect...
+                    if (!effectMapping.ContainsKey(oldEffect))
+                    {
+                        // Make a clone of our replacement effect. We can't just use
+                        // it directly, because the same effect might need to be
+                        // applied several times to different parts of the model using
+                        // a different texture each time, so we need a fresh copy each
+                        // time we want to set a different texture into it.
+                        Effect newEffect = replacementEffect.Clone(replacementEffect.GraphicsDevice);
+
+                        // Copy across the texture from the original effect.
+                        newEffect.Parameters["WarpTexture"].SetValue(warpTexture);
+                        newEffect.Parameters["SpecTexture"].SetValue(specTexture);
+                        newEffect.Parameters["SpecMaskTexture"].SetValue(specMaskTexture);
+                        newEffect.Parameters["Texture"].SetValue(oldEffect.Texture);
+                        newEffect.Parameters["TextureEnabled"].SetValue(oldEffect.TextureEnabled);
+
+                        effectMapping.Add(oldEffect, newEffect);
+                    }
+                }
+
+                // Now that we've found all the effects in use on this mesh,
+                // update it to use our new replacement versions.
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                {
+                    meshPart.Effect = effectMapping[meshPart.Effect];
+                }
+            }
+        }
+
+        /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice); // Create a new SpriteBatch, which can be used to draw textures.
-            colourMap = Content.Load<Texture2D>(@"Textures\color_map");
+            colourMap = Content.Load<Texture2D>(@"Textures/color_map");
+
+            // Load the warp colour (From mitchell p19)
+            warpTexture = Content.Load<Texture2D>(@"Textures/warp");
+            specTexture = Content.Load<Texture2D>(@"Textures/ShipSpecular");
+            specMaskTexture = Content.Load<Texture2D>(@"Textures/ShipSpecularMask");
+
+            // A ship using the default BasicEffect
+            defShip = Content.Load<Model>(@"Models/Ship2");
+
+            // Change the model to use the illustrative shader
+            Effect illustrative = Content.Load<Effect>(@"Shaders/Illustrative");
+            ship = Content.Load<Model>(@"Models/Ship");
+            ChangeModelEffect(ship, illustrative);
         }
 
         /// <summary>
@@ -194,6 +257,7 @@ namespace MobRules
             if (inputManager.CurrentKBState.IsKeyDown(Keys.Escape))
                 this.Exit();
 
+            /*
             // Update the Effect
             Matrix world = Matrix.CreateRotationY(MathHelper.ToRadians(cubeRot.Y)) * Matrix.CreateRotationX(MathHelper.ToRadians(cubeRot.X)) * Matrix.CreateTranslation(cubePos);
 
@@ -207,11 +271,12 @@ namespace MobRules
             phongEffect.LightPosition = new Vector4(camera.Position, 1.0f);
             phongEffect.LightIntensity = 2.0f;
             phongEffect.Shader.CommitChanges();
-
+            */
+             
             base.Update(gameTime);
         }
 
-        private void DrawCube()
+/*        private void DrawCube()
         {
             graphics.GraphicsDevice.VertexDeclaration = vertexDecl;
             graphics.GraphicsDevice.Vertices[0].SetSource(vertexBuffer, 0, VertexPositionNormalTexture.SizeInBytes);
@@ -225,6 +290,7 @@ namespace MobRules
             }
             phongEffect.Shader.End();
         }
+*/
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -232,9 +298,51 @@ namespace MobRules
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            graphics.GraphicsDevice.Clear(Color.Black);
 
-            DrawCube();
+            //DrawCube();
+            
+            Matrix world = Matrix.CreateScale(0.01f) * Matrix.CreateRotationY(MathHelper.ToRadians(shipRot.X)) * Matrix.CreateRotationX(MathHelper.ToRadians(shipRot.Y)) * Matrix.CreateTranslation(shipPos);
+            
+            // Look up the bone transform matrices.
+            Matrix[] transforms = new Matrix[ship.Bones.Count];
+            ship.CopyAbsoluteBoneTransformsTo(transforms);
+
+            System.Console.WriteLine(defShip.Meshes.Count);
+
+            // Draw the model
+            foreach (ModelMesh mesh in ship.Meshes)
+            {
+                foreach (Effect effect in mesh.Effects)
+                {
+                    Matrix locWorld = transforms[mesh.ParentBone.Index] * world;
+                    effect.Parameters["World"].SetValue(locWorld);
+                    effect.Parameters["View"].SetValue(camera.View);
+                    effect.Parameters["Projection"].SetValue(camera.Projection);
+                    effect.Parameters["Camera"].SetValue(camera.Position);
+                    //effect.Parameters["Light"].SetValue(new Vector4(camera.Position, 1.0f));
+                    effect.Parameters["Light"].SetValue(new Vector4(10.0f, 10.0f, 0.0f, 1.0f)); // Top right corner of screen
+                    effect.Parameters["LightColour"].SetValue(new Vector4(0.7f, 0.5f, 0.5f, 1.0f));
+                }
+                mesh.Draw();
+            }
+
+            defShip.CopyAbsoluteBoneTransformsTo(transforms);
+            // Draw the model with default lighting
+            foreach (ModelMesh mesh in defShip.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    Matrix locWorld = transforms[mesh.ParentBone.Index] * world * Matrix.CreateTranslation(0.0f, 0.0f, -25.0f);
+                    be.World = locWorld;
+                    be.View = camera.View;
+                    be.Projection = camera.Projection;
+                    //be.LightingEnabled = true;
+                    //be.DirectionalLight0 = new Vector4(10.0f, 10.0f, 0.0f, 1.0f);
+                }
+                mesh.Draw();
+            }
 
             base.Draw(gameTime);
         }
